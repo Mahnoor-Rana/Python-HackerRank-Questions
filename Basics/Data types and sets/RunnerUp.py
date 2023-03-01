961ade7e7900e08b0c31c9d2d647870121989733
Mahnoor Rana <mahnoorrana2002@gmail.com>
2023-03-01 22:38:34 +0500

    updated

---
+++ Basics/Data types and sets/RunnerUp.py
@@ -0,0 +1,10 @@
+#Find the Runner-Up Score!
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    arr = sorted(arr,reverse= True)
+    i=0
+    for i in arr:
+        if (i < arr[0]):
+             print(i)
+             break

bc8c897a26b66dfe6a86fafc4bf491e21d6c85c4
Mahnoor Rana <mahnoorrana2002@gmail.com>
2023-03-04 10:13:50 +0500

    updated

--- Basics/Data types and sets/setMutations.py
+++ Basics/Data types and sets/setMutations.py
@@ -1,6 +1,6 @@
-# You are given a set  and  number of other sets. These  number of sets have to perform some specific mutation operations on set .
+# You are given a set A and N number of other sets. These N number of sets have to perform some specific mutation operations on set A.
 
-# Your task is to execute those operations and print the sum of elements from set .
+# Your task is to execute those operations and print the sum of elements from set A .
 
 n = (input())
 a = set(map(int,input().split()))

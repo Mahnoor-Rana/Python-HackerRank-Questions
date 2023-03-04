2b3b714ce772c7b06167d40585a8d0de0304d7f1
Mahnoor Rana <mahnoorrana2002@gmail.com>
2023-03-04 11:12:45 +0500

    updated

--- Basics/Data types and sets/NoIdea.py
+++ Basics/Data types and sets/NoIdea.py
@@ -1,16 +1,15 @@
 
 
-
-
-n,m = map(int,input().split())
-x = list(map(int,input().split()))
+n, m = map(int, input().split())
+x = list(map(int, input().split()))
 a = set(map(int, input().split()))
-b = set(map(int,input().split()))
-happiness =  0 
+b = set(map(int, input().split()))
+happiness = 0 
+
 for i in x:
-    if i in x:
-       happiness +=1
-    elif i not in x:
-        happiness -=1
+    if i in a:
+       happiness += 1
+    elif i in b:
+        happiness -= 1
    
 print(happiness)

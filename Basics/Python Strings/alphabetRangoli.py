7593a907e32d62b89da922a6718a27ea0cb7c1ae
Mahnoor Rana <mahnoorrana2002@gmail.com>
2023-03-06 00:02:27 +0500

    updated

---
+++ Basics/Python Strings/alphabetRangoli.py
@@ -0,0 +1,20 @@
+
+
+import string
+def print_rangoli(size):
+    # your code goes here
+    l = string.ascii_lowercase[:size]
+    w = 4*size-3
+    
+    for i in range(size-1,0,-1):
+         line = '-'.join(l[size-1:i:-1] + l[i:size])
+         print(line.center(w,'-'))
+         
+    for i in range(size):
+        line = '-'.join(l[size-1:i:-1]+l[i:size])
+        print(line.center(w,'-'))
+         
+         
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)

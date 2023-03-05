69ee535129eb462532eb784553a0ce7630efb95c
Mahnoor Rana <mahnoorrana2002@gmail.com>
2023-03-05 10:00:52 +0500

    updated

---
+++ Basics/Python Strings/swapCase.py
@@ -0,0 +1,21 @@
+#Complete the swap_case function 
+# swap_case has the following parameters:
+# string s: the string to modify
+
+def swap_case(s):
+    s_1=""
+    for char in s:
+        if char.islower():
+            s_1 += char.upper()
+            
+        elif char.isupper():
+            s_1 += char.lower()
+            
+        else:
+            s_1 += char 
+    return s_1
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)

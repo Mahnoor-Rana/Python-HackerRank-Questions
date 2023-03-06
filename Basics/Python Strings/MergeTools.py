a36a01cd756727cafcc91a1db9215581ae7d75e7
Mahnoor Rana <mahnoorrana2002@gmail.com>
2023-03-06 05:54:31 +0500

    initial commit

--- Basics/Python Strings/MergeTools.py
+++ Basics/Python Strings/MergeTools.py
@@ -1,4 +1,4 @@
-
+# There are three substrings of length  to consider: 'AAA', 'BCA' and 'DDE'. The first substring is all 'A' characters, so . The second substring has all distinct characters, so . The third substring has  different characters, so . Note that a subsequence maintains the original order of characters encountered. The order of characters in each subsequence shown is important.
 
 def merge_the_tools(string, k):
     # your code goes here

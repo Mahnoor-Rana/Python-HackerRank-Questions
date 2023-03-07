b919385594316af871840a7a589c6fd113265bbb
Mahnoor Rana <mahnoorrana2002@gmail.com>
2023-03-07 22:22:44 +0500

    initial commit

--- Basics/Basics/loop.py
+++ Basics/Basics/loop.py
@@ -1,10 +1,7 @@
 if __name__ == '__main__':
     n = int(input())
-    import math
 i = 0
 if n >= i:
-    for num in range(0,n):
-        print(pow(i,2))
-    # for num in range(0 , n ):
-    #     print(num**2)
+    for num in range(0 , n ):
+        print(num**2)
         

b632672342974f26d0d75a34ac85af480fc9a149
Mahnoor Rana <mahnoorrana2002@gmail.com>
2023-03-07 22:15:35 +0500

    initial commit

--- Basics/Basics/loop.py
+++ Basics/Basics/loop.py
@@ -1,7 +1,10 @@
 if __name__ == '__main__':
     n = int(input())
+    import math
 i = 0
 if n >= i:
+    for num in range(0,n):
+        print(math.sq)
     for num in range(0 , n ):
         print(num**2)
         

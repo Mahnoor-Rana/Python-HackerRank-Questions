cc3c00572fd6193bb40344582c706259412e8058
Mahnoor Rana <mahnoorrana2002@gmail.com>
2023-03-24 21:30:53 +0500

    updated

--- Basics/Data types and sets/AvgFunc.py
+++ Basics/Data types and sets/AvgFunc.py
@@ -1,13 +1,12 @@
-def average(array):
+def average_array(array):
     arr = set(array)
     return sum(arr)/len(arr)
-    arr = set(array)
-    return sum(arr)/len(arr)
+    
     
     
 
 #if __name__ == '__main__':
 n = int(input())
 arr = list(map(int, input().split()))
-result = average(arr)
+result = average_array(arr)
 print(result)

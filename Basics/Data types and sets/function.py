3c7d62c3ba3f6fbf0588c7f549722dc9d5742079
Mahnoor Rana <mahnoorrana2002@gmail.com>
2023-03-25 23:32:28 +0500

    updated

--- Basics/Data types and sets/function.py
+++ Basics/Data types and sets/function.py
@@ -1,7 +1,7 @@
 
 # write function
 def print_full_name(first, last):
-    # Write your code here
+    Write your code here
     first = (input())
     last = (input())
     print('Hello'+ " " + first+" "+ last + "! You just delved into python.")

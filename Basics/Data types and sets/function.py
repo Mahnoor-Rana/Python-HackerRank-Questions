2a15afe834e0f405b87c3ddfe98f45a49d14a999
Mahnoor Rana <mahnoorrana2002@gmail.com>
2023-03-25 23:30:00 +0500

    updated

--- Basics/Data types and sets/function.py
+++ Basics/Data types and sets/function.py
@@ -2,8 +2,8 @@
 # write function
 def print_full_name(first, last):
     # Write your code here
-   # first = (input())
-   # last = (input())
+    first = (input())
+    last = (input())
     print('Hello'+ " " + first+" "+ last + "! You just delved into python.")
 if __name__ == '__main__':
     first_name = input().split()

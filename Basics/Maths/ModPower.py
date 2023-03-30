5897bdf87155908b87cda6d7aba7585dd5499a36
Mahnoor Rana <mahnoorrana2002@gmail.com>
2023-03-30 22:50:14 +0500

    updated

--- Basics/Maths/ModPower.py
+++ Basics/Maths/ModPower.py
@@ -1,7 +1,7 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-a,b,c= int(input())
-# b = int(input())
-# c = int(input())
+a= int(input())
+b = int(input())
+c = int(input())
 d = pow(a,b)
 print(d)
 print(pow(a,b,c))

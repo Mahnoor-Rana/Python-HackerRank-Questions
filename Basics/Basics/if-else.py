09709a8ebc9d8fe9dfbadc7b071715642f404738
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2023-04-08 23:21:24 +0500

    updated

--- Basics/Basics/if-else.py
+++ Basics/Basics/if-else.py
@@ -4,7 +4,7 @@ import random
 import re
 import sys
 if __name__ == '__main__':
-    m = int(input().split())
+    m = int(input().strip()) # split() also use this 
     if m%2!=0:
         print("Weird")
     elif m >=2 and m <= 5 or m > 20:

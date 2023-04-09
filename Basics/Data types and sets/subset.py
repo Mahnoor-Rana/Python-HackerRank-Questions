14ac78bc5078aaeaaf9a9b200095c70bdabba48a
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2023-04-09 23:21:08 +0500

    updated

--- Basics/Data types and sets/subset.py
+++ Basics/Data types and sets/subset.py
@@ -2,9 +2,9 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 n = int(input())
 for i in range(n):
-    a_len = int(input())
+    a_len ,b_len= int(input())
     a = set(map(int,input().split()))
-    b_len = int(input())
-    b = set(map(int,input().split()))
+   # b_len = int(input())
+   # b = set(map(int,input().split()))
     print(a.issubset(b))
 

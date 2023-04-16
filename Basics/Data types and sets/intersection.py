88d3eb955b2fef6290aa4c6c6de1dd2799a76462
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2023-04-16 23:32:26 +0500

    updated

--- Basics/Data types and sets/intersection.py
+++ Basics/Data types and sets/intersection.py
@@ -1,5 +1,5 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 n,m = int(input())
 s = set(input().split())
-s = s.intersection(s)
+s = s.intersection(input().split())#s
 print(len(s))

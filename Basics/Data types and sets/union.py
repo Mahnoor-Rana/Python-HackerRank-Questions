1b22a89c43dd0d8e4ca31dd80d9d54aa9fe4112a
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2023-04-18 23:43:23 +0500

    updated

--- Basics/Data types and sets/union.py
+++ Basics/Data types and sets/union.py
@@ -1,7 +1,7 @@
 # The students of District College have subscriptions to English and French newspapers. Some students have subscribed only to English, some have subscribed to only French and some have subscribed to both newspapers.
 
 # You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and the other set is subscribed to the French newspaper. The same student could be in both sets. Your task is to find the total number of students who have subscribed to at least one newspaper.
-n = int(input())
+n,s = int(input())
 s = set(input().split())
 m = int(input())
 s = s.union(input().split())

a1f4994ddd163eb1e894dc716ad28998968bbfc3
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2023-04-22 23:42:03 +0500

    updated

--- Basics/Maths/AngleMBC.py
+++ Basics/Maths/AngleMBC.py
@@ -1,8 +1,8 @@
 #write a program to find angle MBC
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 from math import atan, degrees
-AB = int(input())
-BC = int(input())
+a = int(input())
+b = int(input())
 
 angleC = atan(AB/BC)
 print((round(degrees(angleC))),chr(176),sep='') 

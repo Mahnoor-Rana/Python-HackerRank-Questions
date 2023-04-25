491c294d48db614183eb04302ea8ac46ccfc8a2f
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2023-04-25 23:19:27 +0500

    updated

--- Basics/Python Strings/Name.py
+++ Basics/Python Strings/Name.py
@@ -13,5 +13,5 @@ def print_full_name(first, last):
    # last = (input())
     print('Hello'+ " " + first+" "+ last + "! You just delved into python.")
 if __name__ == '__main__':
-    first_name , l= input()
+    first_name , last_name = input()
     print_full_name(first_name, last_name)

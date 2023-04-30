c95ec8e86ac311ebf76926ba6a7a7788c4d4c240
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2023-04-30 22:20:05 +0500

    updated

--- Basics/Basics/Division.py
+++ Basics/Basics/Division.py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
     a ,b= int(input())
-    print(a//b)// floor method
-    print(a/b)  // simple division
+    print(a//b)# floor method
+    print(a/b)  # simple division

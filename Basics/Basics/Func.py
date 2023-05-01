d8fa2b5e81e7af52573475074ea7353cf4298dd8
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2023-05-01 21:51:23 +0500

    updated

--- Basics/Basics/Func.py
+++ Basics/Basics/Func.py
@@ -1,7 +1,7 @@
 # Write a function
 def is_leap(year):
     leap = False
-    if year % 400 != 0 and year % 100 == 0:
+    if year % 400 != 0 and year % 100 == 0: # for leap year
         leap = False
     elif year % 4 == 0:
         leap = True    

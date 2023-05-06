2c1475632b1298a57ffb20ed7054b5bf0b1222b2
Mahnoor-Rana <mahnoorrana2002@gmail.com>
2023-05-06 23:19:17 +0500

    updated

--- Basics/Python Strings/mutations.py
+++ Basics/Python Strings/mutations.py
@@ -8,5 +8,5 @@ def mutate_string(string, position, character):
 if __name__ == '__main__':
     s = input()
     i, c = input().split()
-    s_new = mutate_string(s, int(i), c)
+    new = mutate_string(s, int(i), c)
     print(s_new)
